91f038ab8c0fb769d86c934e7db83be1d25c3a2f
facusada <facundosadava1@gmail.com>
2020-11-30 10:51:57 -0300

    deploy

---
+++ build/precache-manifest.c88ac78f2e3dbcfcd479348ab5111444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2fd43353fca6c78ea478b5b35bdc4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e219076b94e00e66688",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "e9b9eb326387095c0fa6",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9e219076b94e00e66688",
+    "url": "/static/js/2.5044ce3b.chunk.js"
+  },
+  {
+    "revision": "0274944c907f03fac1236e2ce406bcba",
+    "url": "/static/js/2.5044ce3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b9eb326387095c0fa6",
+    "url": "/static/js/main.7f0693d5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
